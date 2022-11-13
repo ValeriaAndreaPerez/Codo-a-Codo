b2d0caa2efdaa7b8a28dbd6e0f26ab0735c8fbaa
Valeria <valeria.andrea.perez@gmail.com>
2022-11-13 11:28:12 -0300

    version-encuadre

---
+++ Css/big.py
@@ -0,0 +1,17 @@
+nota= int(input("la nota es: "))
+ # Condicionales
+
+if nota>=0 and nota<4:  #And equivalente al && de js
+    print ("Usted a desaprobado")
+elif nota>=4 and nota <7:
+    print("Usted a aprtobado")
+elif nota>=7 and nota <= 10:
+    print("Usted a promocionado")
+else:
+    print("Error")
+
+"contador =1
+while contador < 11:
+    print(contador)
+    contador +=12""
+
